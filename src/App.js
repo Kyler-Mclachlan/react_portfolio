d35555b745213a11780a678c9776baf5f4af3104
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-04-09 13:50:44 -0500

    set up component structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import React, { useState } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Project from './components/Project' 
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
