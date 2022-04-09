eb3b60888295f26ee979ced3217a4bd24fac5b34
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-04-09 18:06:45 -0500

    images looking passable

--- src/components/Pages/ProjectPage/index.js
+++ src/components/Pages/ProjectPage/index.js
@@ -5,37 +5,37 @@ export default function Project() {
 
   const [projects] = useState([
     {
-    //   title: "FavMates",
-    //   link: "https://lit-mountain-78827.herokuapp.com/",
-    // },
-    // {
-      title: "SEO Optimization",
-      link: "https://github.com/Kyler-Mclachlan/Horiseon-SEO-and-Accessibility-Optimization",
+      title: "FavMates",
+      link: "https://lit-mountain-78827.herokuapp.com/",
     },
     {
       title: "Concert Scout",
       link: "https://austinslatey.github.io/concert-scout/",
+    },
+    {
+      title: "Note Taker",
+      link: "https://blooming-citadel-90751.herokuapp.com/",
+    },
+        {
+      title: "Tech Blog",
+      link: "https://afternoon-mountain-91825.herokuapp.com/",
+    },
+    {
+      title: "SEO Optimization",
+      link: "https://github.com/Kyler-Mclachlan/Horiseon-SEO-and-Accessibility-Optimization",
+    },
+    {
+      title: "PWA",
+      link: "https://lychee-cobbler-55273.herokuapp.com/",
     }
-    // {
-    //   title: "Note Taker",
-    //   link: "https://blooming-citadel-90751.herokuapp.com/",
-    // },
-    // {
-    //   title: "PWA",
-    //   link: "https://lychee-cobbler-55273.herokuapp.com/",
-    // },
-    // {
-    //   title: "Tech Blog",
-    //   link: "https://afternoon-mountain-91825.herokuapp.com/",
-    // }
   ]);
 
 
   return (
     //method 1
-    <div className="project">
+    <div className='projectContainer'>
     {projects.map((image, i) => (
-      <img
+      <img className="projectItem"
         src={require(`../../assets/${i}.PNG`)}
         alt={image.title}
         key={image.title}
