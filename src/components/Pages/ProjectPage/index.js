3f29fd7e030469b9554e786d62f355aec32aae93
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-04-09 19:09:52 -0500

    portfolio page kinda working

--- src/components/Pages/ProjectPage/index.js
+++ src/components/Pages/ProjectPage/index.js
@@ -1,47 +1,54 @@
 import React, { useState } from 'react';
-import pic from "../../assets/0.PNG";
-// import photo from '../../assets'
 export default function Project() {
 
   const [projects] = useState([
     {
       title: "FavMates",
       link: "https://lit-mountain-78827.herokuapp.com/",
+      githubLink: "https://github.com/Nate-Huerd/Fav-Mates"
     },
     {
       title: "Concert Scout",
       link: "https://austinslatey.github.io/concert-scout/",
+      githubLink: "https://github.com/austinslatey/concert-scout"
     },
     {
       title: "Note Taker",
       link: "https://blooming-citadel-90751.herokuapp.com/",
+      githubLink: "https://github.com/Kyler-Mclachlan/NoteTaker"
     },
         {
       title: "Tech Blog",
       link: "https://afternoon-mountain-91825.herokuapp.com/",
+      githubLink: "https://github.com/Kyler-Mclachlan/Tech-Blog"
     },
     {
       title: "SEO Optimization",
       link: "https://github.com/Kyler-Mclachlan/Horiseon-SEO-and-Accessibility-Optimization",
+      githubLink: "https://github.com/Kyler-Mclachlan/Horiseon-SEO-and-Accessibility-Optimization"
     },
     {
       title: "PWA",
       link: "https://lychee-cobbler-55273.herokuapp.com/",
+      githubLink: "https://github.com/Kyler-Mclachlan/Budget-Tracker"
     }
   ]);
 
 
   return (
     //method 1
-    <div className='projectContainer'>
+    <div>
     {projects.map((image, i) => (
+      <a href={image.link}>
       <img className="projectItem"
         src={require(`../../assets/${i}.PNG`)}
         alt={image.title}
         key={image.title}
+        href={image.link}
       />
+      <a className='projectTitle' href={image.githubLink}>{image.title}</a>
+  </a>
     ))}
-    {/* method 2 */}
   </div>
   );
 };
